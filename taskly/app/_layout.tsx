225762a1c02815db5c320203e3ff3954ba5feed3
Harshil Patel <harshilp046@gmail.com>
2024-11-01 00:56:28 +0530

    changed navigation bar icons with something realistic and changed it's color.

--- taskly/app/_layout.tsx
+++ taskly/app/_layout.tsx
@@ -1,19 +1,38 @@
 import { Tabs } from "expo-router";
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
+import Feather from '@expo/vector-icons/Feather';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { theme } from "../theme";
 
 export default function Laytout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ tabBarActiveTintColor: theme.colorCerulean}}>
       <Tabs.Screen
         name="index"
-        options={{ title: "Shopping List" }}
+        options={{
+          title: "Shopping List",
+          tabBarIcon: ({ color, size }) => {
+            return <FontAwesome6 name="list-ul" size={size} color={color} />
+          }
+        }}
       />
       <Tabs.Screen
         name="counter"
-        options={{ title: "Counter" }}
+        options={{
+          title: "Counter",
+          tabBarIcon: ({ color, size }) => {
+            return <Feather name="clock" size={size} color={color} />
+          }
+        }}
       />
       <Tabs.Screen
         name="idea"
-        options={{ title: "Idea" }}
+        options={{
+          title: "Idea",
+          tabBarIcon: ({color, size}) => {
+            return <Ionicons name="bulb-outline" size={size} color={color} />
+          }
+        }}
       />
     </Tabs>
   );
