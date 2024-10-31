0e8c9f3616141eaea96016064a14228022fca592
Harshil Patel <harshilp046@gmail.com>
2024-10-31 22:36:17 +0530

    created 2 new files to use as different pages and added navigation to each.

--- taskly/app/index.tsx
+++ taskly/app/index.tsx
@@ -1,10 +1,17 @@
 import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 import { theme } from '../theme';
 import { ShoppingListItem } from '../components/ShoppngListItem';
+import { Link, useRouter } from 'expo-router';
 
 export default function App() {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
+      <TouchableOpacity onPress={() => router.navigate('/idea')}>
+        <Text style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>Go to Idea</Text>
+      </TouchableOpacity>
+      <Link href="/counter" style={{ textAlign: "center", marginBottom: 18, fontSize: 24 }}>Go to Counter</Link>
       <ShoppingListItem name="Whiskey" />
       <ShoppingListItem name="Spirit" isCompleted />
       <ShoppingListItem name="Vodka" isCompleted />
